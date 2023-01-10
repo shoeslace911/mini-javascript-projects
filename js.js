b01ef073576d102a198c0b37656ec2aa9617d34a
Sho Fujiwara <shofujiwara505@gmail.com>
2023-01-10 12:53:00 +0900

    can now click both normal boring colors or epic hex colors

--- js.js
+++ js.js
@@ -1,18 +1,25 @@
 let hexButton = document.querySelector("#hex-change-button");
-let color = ["red", "blue", "yellow", "green", "silver"];
 let hexCodeArray = ["a", "b", "c", "d", "e", "f", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let hexTextField = document.querySelector(".hex-text");
+let colorArray = ["red", "blue", "yellow", "green", "silver"];
+let colorButton = document.querySelector("#color-change-button");
+let colorTextField = document.querySelector(".color-text");
+
+const randomizeNumber = (arrayLength) => {
+  return Math.floor(Math.random() * arrayLength.length);
+};
 
 hexButton.addEventListener("click", () => {
   let hexCode = "#";
   for (let i = 0; i < 6; i++) {
-    hexCode += hexCodeArray[randomizeNumber()];
-    console.log(hexCode);
+    hexCode += hexCodeArray[randomizeNumber(hexCodeArray)];
+    hexTextField.innerHTML = hexCode;
   }
   document.body.style.background = hexCode;
 });
 
-const randomizeNumber = () => {
-  return Math.floor(Math.random() * hexCodeArray.length);
-};
-
-console.log(randomizeNumber());
+colorButton.addEventListener("click", () => {
+  let color = colorArray[randomizeNumber(colorArray)];
+  colorTextField.innerHTML = color;
+  document.body.style.background = color;
+});
